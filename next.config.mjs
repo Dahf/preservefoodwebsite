c282475af62c77b89aa8f826ed337e16256e991f
Silas Beckmann <silas.beckmann@mni.thm.de>
2025-12-25 22:24:03 +0100

    Enhance Next.js configuration by adding Webpack alias for easier path resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   transpilePackages: ["geist"],
   images: {
     remotePatterns: [{ hostname: "localhost" }, { hostname: "randomuser.me" }],
+  },
+  webpack: (config) => {
+    // Fallback-Webpack-Alias, falls TS-Pfadalias in der CI nicht aufgelöst wird
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(process.cwd(), "src"),
+    };
+    return config;
   },
 };
 
